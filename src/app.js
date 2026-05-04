1bc3f60f0183fd38933f1ff567343a5cf5de7cf9
musahidkhn-dev <musahidmk6850@gmail.com>
2026-05-04 17:28:30 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -74,10 +74,10 @@ app.use('/api/admin', adminRoutes);
 
 
 // React static serve 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
 });
 
 // 404 handler
